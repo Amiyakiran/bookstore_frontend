e14f7eddd342490ff58f235725fc91eba5e7e0a1
amiyakiran <amiyakiran@gmail.com>
2025-04-28 10:41:40 +0530

    server url changed

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,2 +1,2 @@
-//  export const serverUrl = "https://bookstore-backend-263y.onrender.com"
- export const serverUrl = "http://localhost:4000"
+ export const serverUrl = "https://bookstore-backend-263y.onrender.com"
+//  export const serverUrl = "http://localhost:4000"
